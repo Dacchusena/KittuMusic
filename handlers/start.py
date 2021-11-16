43dfed1f85a75dafb36ac145a2e5b8454611ecb8
Darshusena <94253420+Dacchusena@users.noreply.github.com>
2021-11-17 00:07:12 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -52,7 +52,7 @@ async def _human_time_duration(seconds):
 )
 async def start_private(client: Client, message: Message):
     await message.reply_text(
-        f"""✨ **Welcome {message.from_user.mention()} !**\n
+        f"""✨ **Welcome [🐱] (https://telegra.ph/file/2c0deee1097a8dab6bab5.jpg),{message.from_user.mention()} !**\n
 💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music on groups through the new Telegram's voice chats!**
 
 💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
